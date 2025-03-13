ed8d3eb65e4a4f23b52800a646fdfd7deb11f018
bsod2528 <bsod2528@proton.me>
2025-03-13 07:29:57 +0530

    added reset to imem

--- design sources/instruction_memory.v
+++ design sources/instruction_memory.v
@@ -5,22 +5,23 @@
 // Have to mention the inspiration for this: https://www.edaplayground.com/x/iCVx
 // Thanks eda playground && whoever wrote that (they won't read but still, it's from the bottom of my heart)
 
-module instruction_memory(clk, address, enable, instruction);
-    input clk, enable; // enable acts like my reset here.
+module instruction_memory(clk, reset, address, enable, instruction);
+    input clk, reset, enable;
     input [15:0] address;
     output reg [15:0] instruction;
-
+    
     reg [15:0] imem [255:0]; // just 256 instructions for now which will.
-
+    
     initial begin
+        instruction = 16'b0000_0000_0000_0000;
         $readmemb("C:/cpu/cpu.srcs/sources_1/new/imem.mem", imem);
-        $display("mem file loaded");
     end
-
-    always @ (posedge clk) begin
-        if (enable)
-            instruction <= imem[address];
-        else
+    
+    always @ (posedge clk or reset) begin
+        $display("AT: %t", $time);
+        if (reset)
             instruction <= 16'b0000_0000_0000_0000;
+        else if (enable)
+            instruction <= imem[address];
     end
 endmodule
