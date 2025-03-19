c56666c8226e0301509184a0a90808b5af5be2eb
bsod2528 <bsod2528@proton.me>
2025-03-19 21:05:21 +0530

    added license header to all `.v` files

--- design sources/instruction_decoder.v
+++ design sources/instruction_decoder.v
@@ -1,3 +1,20 @@
+// 16-CPU: A basic 16-bit RISC processor
+// Copyright (C) 2025 Vishal Srivatsava AV
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 `timescale 1ns / 1ps
 
 // Instruction bits be like:
